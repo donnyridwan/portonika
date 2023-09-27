ee19d2a6c093479eeb28c4e13918c6f5196cebe1
unknown <donnyr65@gmail.com>
2023-09-27 07:14:53 +0700

    First commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,7 +43,7 @@ export default function Home() {
         >
           <a
             className="rounded-[4px] border-2 border-accent px-7 py-5 text-accent transition-all duration-300 hover:bg-accent hover:bg-opacity-10"
-            href="https://drive.google.com/drive/folders/1QoujbRghInMFjV5ECmudG3Mho-aOdngI?usp=sharing"
+            href=""
             target="_blank"
             rel="noreferrer"
           >
